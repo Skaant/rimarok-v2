ca9cd110f04e5c97d7fd67ea7958e8321e53bdac
imrok le bleu <romaric.ruga@gmail.com>
2021-02-12 11:22:05 +0100

    UI : polished pages & base content

--- _data/articles/5/5.article.js
+++ _data/articles/5/5.article.js
@@ -1,6 +1,6 @@
 export default {
   id: '5',
-  title: 'rimarok.com en déploiement continu',
+  title: 'dev.rimarok.com en déploiement continu',
   date: '11/02/2021',
   tags: [ 'rimarok.com', 'devops', 'CI/CD', 'ftp' ],
   description: false
