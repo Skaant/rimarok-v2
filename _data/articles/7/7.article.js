def119d73c6b244a1ae68e89aa7b3387c9710bc6
Rimarok <romaric.ruga@gmail.com>
2021-04-05 18:44:43 +0200

    article 7

---
+++ _data/articles/7/7.article.js
@@ -0,0 +1,7 @@
+export default {
+  id: '7',
+  title: 'Le langage des spécifications',
+  date: '05/04/2021',
+  tags: [ 'court', 'philosophie du code', 'langage', 'communication', 'tests' ],
+  description: false
+}
